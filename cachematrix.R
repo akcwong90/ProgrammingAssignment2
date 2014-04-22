0dcf0ec0a3fc7a91dd6b7ffa4d0daab472e8f818
Andrew Wong <akcwong90@gmail.com>
2014-04-21 21:38:21 -0700

    check in of cachematrix implementation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## Given a numeric vector, constructs a square matrix.  
+## Assumes a dimensions are a squarable value.  Also provides to get and set the inverse
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() matrix(x, sqrt(length(x)), sqrt(length(x)))
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
-
+##  Caches the inverse of matrix and provides its values.
+##  If not, performs a solve to compute it, and caches the result.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached inverse")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
